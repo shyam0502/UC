e9ebc3fb7b10f27ffc4d81fc020acce52c77db65
Shyam <shyam05028@gmail.com>
2026-03-02 08:58:45 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,7 @@
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Printing the literal text "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
