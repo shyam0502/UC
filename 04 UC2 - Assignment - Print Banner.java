bf96c70e18d80fd28bdd0d2ae39239025ea51fbb
Shyam <shyam05028@gmail.com>
2026-03-02 09:07:13 +0530

    Add files via upload

---
+++ 04 UC2 - Assignment - Print Banner.java
@@ -0,0 +1,27 @@
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        
+        System.out.println("=================================");
+        System.out.println("      OOPS BANNER - UC2");
+        System.out.println("=================================");
+        System.out.println();
+        
+        // Line 1: Top border of all letters
+        System.out.println(" ***   ***   *****   *****");
+        
+        // Lines 2-6: Middle sections (all identical for these letters)
+        System.out.println("*   * *   * *     * *     *");
+        System.out.println("*   * *   * *     * *     *");
+        System.out.println("*   * *   * *     * *     *");
+        System.out.println("*   * *   * *     * *     *");
+        System.out.println("*   * *   * *     * *     *");
+        
+        // Line 7: Bottom border of all letters
+        System.out.println(" ***   ***   *****   *****");
+        
+        System.out.println();
+        System.out.println("=================================");
+    }
+}
